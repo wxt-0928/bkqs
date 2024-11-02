cb8358d2461ed7a95d24742b4f70a527030a8d67
wxt-0928 <2982364977@qq.com>
2024-11-02 23:30:38 +0800

    增删查改后台

--- sdbkqs/springboot/src/main/java/com/example/service/UserService.java
+++ sdbkqs/springboot/src/main/java/com/example/service/UserService.java
@@ -1,11 +1,19 @@
 package com.example.service;
 
-
+import cn.hutool.core.util.ObjectUtil;
+import com.example.common.Constants;
+import com.example.common.enums.ResultCodeEnum;
+import com.example.common.enums.RoleEnum;
+import com.example.entity.Admin;
 import com.example.entity.User;
+import com.example.exception.CustomException;
 import com.example.mapper.UserMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -14,9 +22,51 @@ public class UserService {
     private UserMapper userMapper;
 
     public void add(User user) {
+        // 业务方法
+        // 1. 判断用户账号是否重复
+        User dbUser = userMapper.selectByUsername(user.getUsername());
+        if (dbUser != null) {
+            throw new CustomException(ResultCodeEnum.USER_EXIST_ERROR);
+        }
+        // 2. 判断用户密码是不是空
+        if (ObjectUtil.isEmpty(user.getPassword())) {
+            user.setPassword(Constants.USER_DEFAULT_PASSWORD); // 默认密码 123
+        }
+        // 3. 判断用户名称是不是空
+        if (ObjectUtil.isEmpty(user.getName())) {
+            user.setName(user.getUsername());
+        }
+        // 4. 默认用户角色
+        user.setRole(RoleEnum.USER.name());
         userMapper.insert(user);
     }
 
+    public void deleteById(Integer id) {
+        userMapper.deleteById(id);
+    }
 
+    public void deleteBatch(List<Integer> ids) {
+        for (Integer id : ids) {
+            this.deleteById(id);
+        }
+    }
+
+    public void updateById(User user) {
+        userMapper.updateById(user);
+    }
+
+    public User selectById(Integer id) {
+        return userMapper.selectById(id);
+    }
+
+    public List<User> selectAll(User user) {
+        return userMapper.selectAll(user);
+    }
+
+    public PageInfo<User> selectPage(User user, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<User> list = userMapper.selectAll(user);
+        return PageInfo.of(list);
+    }
 
 }
