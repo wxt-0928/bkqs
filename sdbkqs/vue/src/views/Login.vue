e9a6ae65cd9f8e53ced0a1eeb7d389aebcf91ec2
wxt-0928 <2982364977@qq.com>
2024-11-04 11:15:16 +0800

    c

--- sdbkqs/vue/src/views/Login.vue
+++ sdbkqs/vue/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div style="width: 380px; padding: 50px 30px; background-color: white; border-radius: 5px;">
-      <div style="text-align: center; font-size: 24px; margin-bottom: 30px; color: #333">博客平台</div>
+      <div style="text-align: center; font-size: 24px; margin-bottom: 30px; color: #333">师大百科全书</div>
       <el-form :model="form" :rules="rules" ref="formRef">
         <el-form-item prop="username">
           <el-input size="medium" prefix-icon="el-icon-user" placeholder="请输入账号" v-model="form.username"></el-input>
